50ea0c49427622fe10e6711bc49d2d1895740283
Peadar Grant <peadargrant@gmail.com>
2024-02-13 13:14:42 +0000

    world cities loader script

--- datasets/load_worldcities.sql
+++ datasets/load_worldcities.sql
@@ -2,21 +2,16 @@ drop table if exists cities;
 
 create table cities (
 
-       city text not null,
-       city_ascii text not null,
-       lat float not null,
-       lng float not null,
-       country text not null,
-       iso2 text not null,
-       iso3 text not null,
-       admin_name text not null,
-       capital text not null,
-       population numeric,
-       id int not null primary key
+       city,
+       city_ascii,
+       lat,
+       lng,
+       country,
+       iso2,
+       iso3,
+       admin_name,
+       capital,
+       population,
+       id
 
 );
-
-\COPY cities FROM 'worldcities.csv' WITH ( FORMAT CSV, DELIMITER ',', HEADER, FORCE_NULL(population));
-
-COMMENT ON TABLE cities IS 'Data from worldcities.csv at https://simplemaps.com/data/world-cities';
-
